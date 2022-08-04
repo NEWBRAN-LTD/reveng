200d2ccdf03d118d660ea56d62f2f0937fc81cdc
Joel Chu <joelckchu@gmail.com>
2022-08-04 21:41:21 +0800

    trace path is working

--- packages/core/src/tracer.ts
+++ packages/core/src/tracer.ts
@@ -1,8 +1,9 @@
 // this will act like a bunlder to map out all the imports and each files
 // calls and parameters and how they related to each other for Reverse Engineers
-import type { AnyType } from './types'
-import { swcParserSync } from './lib/swc-parser-base'
-import { getOptions } from './lib/common'
+import type { AnyType } from '@jsonql/ast/index'
+import { swcParserSync } from '@jsonql/ast/dist/parsers'
+import fsx from 'fs-extra'
+import { resolve, join, extname } from 'node:path'
 
 /* the main call to the swc */
 export function tracer (pathToFile: string, options = {}) {
@@ -22,9 +23,45 @@ function extractImports (ast: Array<AnyType>): Array<AnyType> | boolean {
 }
 
 /* when we get the import path then we need to workout the full path to the import file */
-function getFullPathToImport (importPath: string): string {
-
+export async function getFullPathToImport (importPath: string): Promise<string> {
+  const _path = resolve(importPath)
+  if (fsx.existsSync(_path)) {
+    return getFileStat(_path)
+  }
+  // is there an extension?
+  const ext = extname(_path)
+  if (ext) {
+    return Promise.reject(`${_path} not found!`)
+  }
+  // @TODO should we include the other extensions like cjs / mjs / js ? 
+  return getFileStat([_path, 'ts'].join('.'))
 }
+
+/** The actual method to get the file path */
+async function getFileStat (importPath: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fsx.stat(importPath, (err: Error, stats: AnyType) => {
+      if (err) {
+        return reject(err)
+      }
+      if (stats.isDirectory()) {
+        // just do this in a dumb way
+        const p1 = [importPath, 'ts'].join('.')
+        if (fsx.existsSync(p1)) {
+          return resolve(p1)
+        }
+        const p2 = join(importPath, 'index.ts')
+        if (fsx.existsSync(p2)) {
+          return resolve(p2)
+        }
+      } else if (stats.isFile()) {
+        return resolve(importPath)
+      }
+      reject(`${importPath} not found!`)
+    })
+  })
+}
+
 
 
 /* we only want the body part */
@@ -39,8 +76,8 @@ function extractBody (ast: AnyType) {
 
 /* fileToAst low level code to rip the input path file to AST */
 function pathToAst (pathToFile: string, options: AnyType) {
-  const opts = getOptions('ts')
+
   const defaultTarget = { target: 'es2021' } // set a default target
 
-  return swcParserSync(pathToFile, Object.assign(opts, defaultTarget, options))
+  return swcParserSync(pathToFile, Object.assign(defaultTarget, options))
 }
--- packages/core/tests/tracer.test.ts
+++ packages/core/tests/tracer.test.ts
@@ -2,13 +2,23 @@
 import test from 'ava'
 import { join } from 'node:path'
 
-import { tracer } from '../src/tracer'
+import { tracer, getFullPathToImport } from '../src/tracer'
 
 const mainFile = join(__dirname, 'fixtures', 'trace', 'index.ts')
 
+test('Testing the getFullPathToImport', async t => {
+  const target1 = join(__dirname, 'fixtures', 'trace')
+  const target2 = join(__dirname, 'fixtures', 'trace', 'lib')
 
-test(`Should able to return an ast file`, t => {
-  const ast = tracer(mainFile, { target: 'es2021'})
+  const result1 = await getFullPathToImport(target1)
+  const result2 = await getFullPathToImport(target2)
+
+  t.true(result1.indexOf(join('trace', 'index.ts')) > -1)
+  t.true(result2.indexOf(join('trace', 'lib.ts')) > -1)
+})
+
+test.skip('Should able to return an ast file', t => {
+  const ast = tracer(mainFile)
 
   console.dir(ast, { depth: null })
 
