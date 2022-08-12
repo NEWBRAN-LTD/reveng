50625a505ec00fb4d8af3b8f02c6b1bf6751eb97
Joel Chu <joelckchu@gmail.com>
2022-08-12 20:51:54 +0800

    @jsonql/utils update to 1.3.2

--- packages/core/src/tracer.ts
+++ packages/core/src/tracer.ts
@@ -3,6 +3,7 @@
 import type { AnyType, SpecifiersNode } from '../'
 import { swcParserSync } from '@jsonql/ast/dist/parsers'
 import { isEmptyObj } from '@jsonql/utils/dist/empty'
+import { processAll } from '@jsonql/utils/dist/promise'
 import fsx from 'fs-extra'
 import { resolve, join, extname } from 'node:path'
 
@@ -44,6 +45,42 @@ function extractImportProps (ast: Array<AnyType>) {
     ))
 }
 
+/** once we grab all the import paths we try to resolve them with the real path
+and travel to the next */
+export function resolveImportsToPaths (
+  imports: Array<string>,
+  basePath?: string // how do we work out the base path?
+) {
+  return processAll(
+    imports.map((i: string) => (
+      getFullPathToImport(i, basePath)
+    ))
+  ).then((results: Array<AnyType>) => {
+    const [ pass, fail ] = results
+    // we have failed find paths
+    if (fail.length > 0) {
+      return processAll(fail.map((f: string) => {
+        const nodeModulePath = isNodeModule(f)
+        if (nodeModulePath !== false) {
+          return {
+            full: nodeModulePath,
+            path: f
+          }
+        }
+        return {
+          notFound: true,
+          path: f
+        }
+      }))
+      .then((results1) => {
+
+      })
+    }
+
+    return results // just return the results
+  })
+}
+
 /* the real mean here we need to trace the import and go to the next file */
 export function extractImportsTree (
   ast: Array<AnyType>,
@@ -78,18 +115,19 @@ export async function getFullPathToImport (importPath: string, basePath?: string
       return getFileStat(search)
     }
   }
-  // This could be a node_modules, we fail it here and let the next method to deal with it
-  return Promise.reject(`${_path} not found!`)
+  // This could be a node_modules, we fail it here and let the ne
+  return Promise.reject(importPath) // just return the path
 }
 
 /** check the node_module to see if the import existed there */
-function isNodeModule (importPath: string): boolean {
+function isNodeModule (importPath: string): boolean | string {
   // @NOTE we use the process.cwd() as the root
   // but this could be wrong
   const root = process.cwd()
   const nodeModulePath = join(root, 'node_modules')
+  const searchPath = join(nodeModulePath, importPath)
 
-  return fsx.existsSync(join(nodeModulePath, importPath))
+  return fsx.existsSync(searchPath) ? searchPath : false
 }
 
 /** The actual method to get the file path */
